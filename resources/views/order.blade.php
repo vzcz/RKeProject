87710488f8cb2dafb8374833c18edf536ff4ae04
Ali <vlaoya33@gmail.com>
2022-08-06 21:23:55 +0300

    Initial commit

---
+++ resources/views/order.blade.php
@@ -0,0 +1,38 @@
+@extends('template.header')
+@section('content')
+    <link rel="stylesheet" href="{{asset('css/order.css')}}">
+
+<form action="" >
+    <fieldset>
+    <div dir="rtl">
+        <div class="order-box">
+            <label for="">الاسم:</label><br>
+            <input type="text" name="name" required>
+        </div>
+        <div class="order-box">
+        <label for="">رقم الجوال:</label><br>
+        <input type="text" name="mobile" required>
+        </div>
+        <div class="order-box">
+        <label for="">التاريخ:</label><br>
+        <input type="datetime-local" name="date" required>
+        </div>
+        <div class="order-box">
+        <label for="">الجنسيه:</label><br>
+        <input type="text" name="Country" required>
+        </div>
+        <div class="order-box">
+            <label for="">الموقع:</label><br>
+            <input type="text" id="#pac-input" name="location" disabled>
+            <div id="map" ></div>
+        </div>
+        <div>
+            <input type="submit" value="ارسال">
+        </div>
+    </div>
+    </fieldset>
+</form>
+<script src="{{asset('js/map.js')}}"></script>
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDKZAuxH9xTzD2DLY2nKSPKrgRi2_y0ejs&libraries=places&callback=initAutocomplete&language=ar&region=EG
+         async defer"></script>
+@stop
---
+++ resources/views/template/header.blade.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="ar">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <title>Service Disk</title>
+</head>
+<body>
+
+
+<!-- As a heading -->
+<nav class="navbar navbar-dark bg-dark">
+    <span class="navbar-brand mb-0 h1">Service Disk</span>
+</nav>
+
+
+@yield('content')
+
+
+<footer class="bg-light text-center text-lg-start">
+    <!-- Copyright -->
+    <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+        © 2020 Copyright:
+        <a class="text-dark" href="https://mdbootstrap.com/">Servicedisk.com</a>
+    </div>
+    <!-- Copyright -->
+</footer>
+
+
+</body>
+</html>
