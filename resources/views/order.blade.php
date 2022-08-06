68d968099c7073d353752e8efcf6dfc73ba03f8a
Ali <vlaoya33@gmail.com>
2022-08-06 21:59:33 +0300

    first comment

--- resources/views/order.blade.php
+++ resources/views/order.blade.php
@@ -33,6 +33,6 @@
     </fieldset>
 </form>
 <script src="{{asset('js/map.js')}}"></script>
-<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDKZAuxH9xTzD2DLY2nKSPKrgRi2_y0ejs&libraries=places&callback=initAutocomplete&language=ar&region=EG
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBZYVTmpHgciweQf-J4bPiHJ_F9M6lfXeU&libraries=places&callback=initAutocomplete&language=ar&region=EG
          async defer"></script>
 @stop
